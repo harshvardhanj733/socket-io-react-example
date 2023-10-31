6f8e92e920f4c68d6a13c508015d6272b423fc20
harshvardhanj733 <harshvardhanj733@gmail.com>
2023-10-31 14:44:28 +0530

    Settings.js changedd

--- client/src/agora/Settings.js
+++ client/src/agora/Settings.js
@@ -1,7 +1,6 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
-const token =
-  "007eJxTYMh5bDTzXe9JwS9/N3BzqTdvasyUafgyWds85ZLlPHbptT4KDIkWSRZGRhbJSQYW5iam5omJliYmRmmWhsapiSaGaYlpZkfNUxsCGRk+blvBzMgAgSA+C0NuYmYeAwMAPO0fVg==";
+const token = "007eJxTYMhZu3XHviczUrc8vC2V6pNT33uT2yozJvrhZ//VCdufuH1RYEi0SLIwMrJITjKwMDcxNU9MtDQxMUqzNDROTTQxTEtM43GzSm0IZGRY1bKRhZEBAkF8FobcxMw8BgYAAoQhQg==";
 const appId = "a8b8228cb087457aa9442f913ea41faf";
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
